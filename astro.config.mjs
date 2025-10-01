9200573a7fa354a6b7f602cbb3f6328a221d85a9
Simon Anthony <antsmo@veeqo.com>
2025-10-01 23:27:22 +0100

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  redirects: {
+    '/': '/standings/elite',
+    '/standings': '/standings/elite'
+  },
+  site: 'https://antsmo.github.io',
+  base: '/iracing-standings',
+});
